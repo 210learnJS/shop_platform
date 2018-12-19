b9ca87da1bed377b8b57ba751dbab1f7b4331a89
SherryLin <1043856716@qq.com>
2018-12-19 20:44:22 +0800

    项目初始化

--- server/controllers/action.js
+++ server/controllers/action.js
@@ -1,8 +1,8 @@
 'use strict';
-
+const fs = require('fs');
 //import { successRes, errorRes } from '../util/res.js'
 const Res = require('../util/res.js');
-var { successRes, errorRes } = Res;
+const { successRes, errorRes } = Res;
 const { USER_TABLE } = require('../constants/constant.js');
 
 const Database = require('../model/contactDB.js');
@@ -16,42 +16,42 @@ DB.startDatabase(function(){
 
 //首页
 async function index(ctx, next){ 
-  try{
-      ctx.response.type = 'text/html';
-      let res = '<h1>Hello world!</h1>';
-      return ctx.body = res;
-  }catch(err){
-      return ctx.body = errorRes('', `${err}`)
-  }
+  	try{
+		const data = await fs.readFileSync('./client/pages/index/index.html');
+		ctx.response.type = 'text/html';
+		return ctx.body = data;
+	}catch(err){
+		return ctx.body = errorRes('', `${err}`)
+	}
 }
 
 //添加
 async function add(ctx, next){
-  try{
-    let param = ctx.request.query;
-    var person = {
-      username: param.username,
-      password: param.password
-    }
-    const result = await DB.add(USER_TABLE, person);
-    const response = result?successRes('添加数据成功'):errorRes('添加数据失败');
-    console.log("add result: ",response);
-    return ctx.body = response;
+  	try{
+		let param = ctx.request.query;
+		var person = {
+			username: param.username,
+			password: param.password
+		}
+		const result = await DB.add(USER_TABLE, person);
+		const response = result?successRes('添加数据成功'):errorRes('添加数据失败');
+		console.log("add result: ",response);
+		return ctx.body = response;
   }catch(err){
-    return ctx.body = errorRes('', `${err}`);
+    	return ctx.body = errorRes('', `${err}`);
   } 
 }
 //删除
 async function del(ctx, next){
-  try{
-    const param = ctx.request.query;
-    const result = await DB.del(USER_TABLE, param);
-    const response = result?successRes('删除数据成功'):errorRes('删除失败，未检索到数据');
-    console.log("delete result: ", response);
-    return ctx.body = response;
-  }catch(err){
-    return ctx.body = errorRes('', `${err}`);
-  } 
+	try{
+		const param = ctx.request.query;
+		const result = await DB.del(USER_TABLE, param);
+		const response = result?successRes('删除数据成功'):errorRes('删除失败，未检索到数据');
+		console.log("delete result: ", response);
+		return ctx.body = response;
+	}catch(err){
+		return ctx.body = errorRes('', `${err}`);
+	} 
 }
 //修改
 async function update(ctx, next){
@@ -70,8 +70,6 @@ async function update(ctx, next){
 //搜索
 async function search(ctx, next){
   try{
-    ctx.set("Access-Control-Allow-Origin", "*");
-
     let param = ctx.request.query;
     const result = await DB.search(USER_TABLE, param);
     const response = result.length>0?successRes(result):errorRes('搜索数据失败');
