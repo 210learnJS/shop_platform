682fde8382a616e6b36c829e3c03be3299e35e02
LeicaYi <1015040930@qq.com>
2018-12-29 16:44:22 +0800

    更新loninPage

--- client/pages/login/login.js
+++ client/pages/login/login.js
@@ -5,18 +5,49 @@
 */
 
 
+var { PATH, PORT } = constant;
+var ipt = document.getElementsByTagName('input');
+var tips = document.getElementById('warn');
+var account = ipt[0];
+var password = ipt[1];
+var btn = document.getElementById('btn');
 
+function verify(name,pwd){
+    if(name==""||pwd==""){
+        return false;
+    }
+    return true;
+}
 
-// var { PATH, PORT } = constant;
-   
-// var butt=document.getElementById("1");
-// butt.addEventListener("click",function(){
-//     var n = document.getElementById("input").value;
-//     n = Number(n, 10);
-//     http.get( PATH + "/search" , {id: n}, function(err, data){
-//         if(!err){
-//             console.log(data);
-//             document.getElementById("div").innerHTML = "username:"+data.result[0].username+" password:"+data.result[0].password;
-//         }
-//     });
-// });
+btn.addEventListener('click',function(){
+    var param = {
+        username: ipt[0].value,
+        password: ipt[1].value,
+    }
+    if(verify(param.username,param.password)){
+        http.post( PATH +"/login", param,function(err,data){
+            // if(!err && data.status.code !=0){
+            //     window.location.href = 'http://localhost:8888/pages/index/index.html';
+            // }else {
+            //     alert("用户名不存在或密码错误，请重新输入");
+            // }
+            if(!err){
+                if(data.usernameExist){
+                   if(data.passwordOk){
+                    window.location.href = 'http://localhost:8888/pages/index/index.html';
+                   }else{
+                       tips.innerHTML = "密码错误";
+                       tips.className = "warn";
+                   }
+                }else{
+                    tips.innerHTML = "用户名不存在";
+                    tips.className = "warn";
+                }
+            }
+        })
+    }else{
+        tips.innerHTML = "输入不能为空";
+        tips.className = "warn";
+    }
+})
+
